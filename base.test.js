be5d92571c9000f25d3f777d51bc2ada8dcdc799
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-02-05 23:53:29 -0500

    Add test folder

--- base.test.js
+++
@@ -1,7 +0,0 @@
-const base = require('./base.js');
-require('dotenv').config()
-
-test ('show greeting test', () => {
-  let expected = `Greetings from foo! ${global.env.GREET} and ${global.env.MAIN}` 
-  expect(base()).toBe(expected);
-});
