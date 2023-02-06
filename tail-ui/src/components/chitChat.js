fe2483e25afdcf2c82c4626209b53c5d67bbf993
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-02-05 23:42:08 -0500

    Add test for component

--- tail-ui/src/components/chitChat.js
+++ tail-ui/src/components/chitChat.js
@@ -3,13 +3,13 @@ import logo from '../logo.svg'
 function ChitChat() {
   return (
     <div>
-      <div class="p-6 max-w-sm mx-auto bg-yellow-300 rounded-xl shadow-xl flex items-center space-x-4">
+      <div className="p-6 max-w-sm mx-auto bg-yellow-300 rounded-xl shadow-xl flex items-center space-x-4">
         <div>
           <img className="h-12 w-12" src={logo} alt="ChitChat logo"/>
         </div>
         <div>
-          <div class="text-xl font-medium text-black">ChitChat</div>
-          <p class="text-slate-500">You have a new message!</p>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-slate-500">You have a new message!</p>
         </div>
       </div>
     </div>
